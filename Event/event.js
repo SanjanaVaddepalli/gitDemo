750aeff9f33cf57588622b209b708e0f0770090f
SanjanaVaddepalli <vaddepallisanjana@gmail.com>
2025-10-04 13:00:03 +0530

    Files Added

---
+++ Event/event.js
@@ -0,0 +1,12 @@
+// function f1()
+// {
+//     alert("Hi Coders..")
+// }
+// f1();
+
+
+
+
+document.getElementById("clickMeButton").onclick = function() {
+    document.getElementById("message").innerText = "Button clicked!";
+};
